08dc08ca1d8ea978c5e09f1e3162388b4ce6c0dc
Joel-Mendez0 <113056408+Joel-Mendez0@users.noreply.github.com>
2024-09-03 20:32:53 -0400

    Update master.c

--- SPI/Master/main/master.c
+++ SPI/Master/main/master.c
@@ -1,18 +1,33 @@
 #include "SPI.h"
 #include "freertos/FreeRTOS.h"
 
-uint8_t data[256];
+uint8_t data[1000];
+uint8_t receivedData[1000];
+
+void printbuffer(uint8_t *buffer, uint32_t length){
+	for(uint32_t i = 0; i < length; i++){
+		printf("buffer[%lu] = %u\n",i,buffer[i]);
+	}
+}
 
 void app_main(void) {
-	
-	for (int i = 0; i < 256; i++) {
-		data[i] = i;
-		}
-    INITSPI();
 
-    while(true){
+    for (int i = 0; i < 1000; i++) {
+        data[i] = i % 256;
+    }
+
+    INITSPI();
+    
+    while(1){
+	
+		MASTERTRANSMIT(data, 1000);	
+			
+		vTaskDelay(pdMS_TO_TICKS(1000));
 		
-		TRANSMIT(data, 256);
-	}
-       
-}
+		MASTERRECEIVE(receivedData, 1000);
+		
+		printbuffer(receivedData,1000);
+		
+		vTaskDelay(pdMS_TO_TICKS(1000));
+		
+}}

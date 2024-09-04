d668d52e43f801c73350a55759cd33958939d48f
Joel-Mendez0 <113056408+Joel-Mendez0@users.noreply.github.com>
2024-09-03 20:32:36 -0400

    Update slave.c

--- SPI/Slave/main/slave.c
+++ SPI/Slave/main/slave.c
@@ -1,20 +1,33 @@
-#include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include "SPI.h"
-#include "driver/gpio.h"
+
+uint8_t data[1000];
+uint8_t receivedData[1000];
+
+void printbuffer(uint8_t *buffer, uint32_t length){
+	for(uint32_t i = 0; i < length; i++){
+		printf("buffer[%lu] = %u\n",i,buffer[i]);
+	}
+}
 
 void app_main(void) {
-	
+
+    for (int i = 0; i < 1000; i++) {
+        data[i] = i % 256;
+    }
     INITSPI();
     
     while (true) {
-				
-        uint8_t receivedByte = READBYTE();
-        printf("Received Data: %u\n",receivedByte);
-       
+        
+        SLAVERECEIVE(receivedData, 1000);
+        
+        printbuffer(receivedData,1000);
+        
+        vTaskDelay(pdMS_TO_TICKS(1000));
     	
-    	vTaskDelay(pdMS_TO_TICKS(100)); 
+    	SLAVETRANSMIT(data, 1000);
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
         
     }
-
 }
